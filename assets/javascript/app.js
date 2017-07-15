4f4c2c2afc632e5399ede88027975e60ae5e71dc
Tanner Smith <smithtannera@wifi_stu-10-38-45-96.uncc.edu>
2017-07-15 13:49:43 -0400

    game files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,15 +2,17 @@ $(document).ready(function(){
 
 
 var APIKey = "a5gkHawN_9QRfm3-xbiEukr6RH0quaTi";	
-var APISecret = "T6m_6i7TTZZU0t8Ldq9-RnMDYGd64k9B";	
+var APISecret = "T6m_6i7TTZZU0t8Ldq9-RnMDYGd64k9B";
+var image1 = "assets/images/rgos1.jpg";	
 
+function jax(){
 	$.ajax({
         type: 'POST',
         url: 'https://api-us.faceplusplus.com/facepp/v3/detect',
         crossDomain: true,
         cache: false,
         async: true,
-        data: { "api_key": APIKey, "api_secret": APISecret },
+        data: {"api_key": APIKey, "api_secret": APISecret, "image_file": image1},
         dataType: 'jsonp',
         success: function(responseData) {
            alert(JSON.stringify(responseData));
@@ -19,11 +21,12 @@ var APISecret = "T6m_6i7TTZZU0t8Ldq9-RnMDYGd64k9B";
             alert(JSON.stringify(responseData));
         }
     });
+};
+
+jax(); 
 
 });
 
-
-});
 
 
 
