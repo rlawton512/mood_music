46ed56e91cc427e1a1600be827b1ffe5db087768
Tanner Smith <smithtannera@wifi_stu-10-38-42-144.uncc.edu>
2017-07-18 19:23:35 -0400

    game files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,10 +5,11 @@ var APIKey = "a5gkHawN_9QRfm3-xbiEukr6RH0quaTi";
 var APISecret = "T6m_6i7TTZZU0t8Ldq9-RnMDYGd64k9B";
 var image1 = "assets/images/rgos1.jpg";	
 
+//sample jax1
 function jax(){
 	$.ajax({
         type: 'POST',
-        url: 'https://api-us.faceplusplus.com/facepp/v3/detect',
+        url: 'https://api-us.faceplusplus.com/facepp/v3/face/analyze',
         crossDomain: true,
         cache: false,
         async: true,
@@ -23,10 +24,73 @@ function jax(){
     });
 };
 
-jax(); 
+//jax(); 
+
+//sample jax 2
+function jax(){
+    $.ajax({
+        url: "https://api-us.faceplusplus.com/facepp/v3/face/analyze",
+        beforeSend: function(xhr) { 
+        xhr.setRequestHeader("Authorization", "Basic " + btoa("username:password")); 
+        },
+        type: 'POST',
+        dataType: 'json',
+        contentType: 'application/json',
+        processData: false,
+        data: {"api_key": APIKey,
+               "api_secret": APISecret,
+               "image_file": image1},
+        success: function (data) {
+            alert(JSON.stringify(data));
+        },
+        error: function(){
+            alert("Cannot get data");
+        }
+    });
+};
+
+//jax();
+
+
+//from postman
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://api-us.faceplusplus.com/facepp/v3/detect?api_secret=T6m_6i7TTZZU0t8Ldq9-RnMDYGd64k9B&api_key=a5gkHawN_9QRfm3-xbiEukr6RH0quaTi&image_url=https%3A%2F%2Fstatic01.nyt.com%2Fimages%2F2016%2F01%2F12%2Farts%2Fmusic%2F20160112_BOWIE_HP-slide-DMXR%2F20160112_BOWIE_HP-slide-DMXR-videoSixteenByNine3000-v2.jpg",
+  "method": "POST",
+  "headers": {
+    "Access-Control-Allow-Origin": true,
+    "cache-control": "no-cache",
+    "postman-token": "5da6cb75-455b-d385-a67a-a1bfee6c6223"
+  }
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://api-us.faceplusplus.com/facepp/v3/face/analyze?api_key=a5gkHawN_9QRfm3-xbiEukr6RH0quaTi&api_secret=T6m_6i7TTZZU0t8Ldq9-RnMDYGd64k9B&face_tokens=3ddd5eb1630d9d8d5b2a6e9d0e9e14e2&return_attributes=gender%2Cage%2Cethnicity%2Cemotion%2Ceyestatus",
+  "method": "POST",
+  "headers": {
+    "Access-Control-Allow-Origin": true,
+    "cache-control": "no-cache",
+    "postman-token": "357af3aa-6c97-a0ed-3b89-066f013c8159"
+  }
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
 
 });
 
-
-
+// example on the website 
+//curl -X POST "https://api-us.faceplusplus.com/facepp/v3/face/analyze" \
+//-F "api_key=<api_key>" \
+//-F "api_secret=<api_secret>" \
+//-F "return_landmark=1" \
+//-F "face_tokens=c2fc0ad7c8da3af5a34b9c70ff764da0"
 
