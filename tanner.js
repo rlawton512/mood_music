d5104b1627ce5cf1aa14076735b5f779bed05ecb
Muhammad <mchaudh2@uncc.edu>
2017-07-22 13:07:49 -0400

    adding api

--- tanner.js
+++ tanner.js
@@ -1,9 +1,11 @@
 //grab input for image url
 //perform ajax call to SkyBio
+
+
 $('#submitImgUrl').on('click',function(){
 	var userUrl = $('#inputImgUrl').val().trim();
-	var APIKey='dae956b6026544528d9a145149191299';
-	var APISecret = '3d1c30b4f5d746dab0f467e44a1041f4';
+	var APIKey='3a9ca945f04a44bf8f8767ff021d37bc';
+	var APISecret = 'affb8c62eade4475bea1118afb5aa2cf';
 	var settings = {
   "async": true,
   "crossDomain": true,
@@ -62,11 +64,14 @@ $('#submitImgUrl').on('click',function(){
 		mostFitting.push(mostFittingA);
 		mostFitting.push(mostFittingB);
 		console.log(mostFitting);
+		var songsDiv = $('<div>');
+
+
 	
 
 	//split music call into conditions based on emotional results
 	if (mostFitting[1]==="happiness"||mostFitting[1]==="surprise"){
-		var queryURL = "http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=happy&limit=3&api_key=b164494922abda22f8cd2e53cc25ab4e&format=json"
+		var queryURL = "http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=happy&limit=10&api_key=b164494922abda22f8cd2e53cc25ab4e&format=json"
 		 
 		$.ajax({
 			url: queryURL,
@@ -75,9 +80,11 @@ $('#submitImgUrl').on('click',function(){
 			var musicResponse = response
 			console.log("success got data", musicResponse);
 			var randomNo = Math.floor(Math.random()*10);
-			console.log(musicResponse.tracks.track[randomNo].url)
-			$(newDiv).append(`<a href=${musicResponse.tracks.track[randomNo].url}> ${musicResponse.tracks.track[randomNo].artist.name +", "+ musicResponse.tracks.track[randomNo].name}</a>`);
-			$('#resultsDiv').prepend(newDiv);		
+			for (var i = 0; i < 9; i++) {
+				songsDiv.append(`<a href=${musicResponse.tracks.track[i].url} target="_blank"> ${musicResponse.tracks.track[i].artist.name +", "+ musicResponse.tracks.track[i].name}</a><br>`);
+				$('#resultsDiv').html(newDiv);
+				$('#displaySongs').html(songsDiv);	
+			}	
 		})
 	}else if(mostFitting[1]==="anger"||mostFitting[1]==="disgust"){
 		var queryURL = "http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=angry&limit=10&api_key=b164494922abda22f8cd2e53cc25ab4e&format=json"
@@ -89,9 +96,11 @@ $('#submitImgUrl').on('click',function(){
 			var musicResponse = response
 			console.log("success got data", musicResponse);
 			var randomNo = Math.floor(Math.random()*10);
-			console.log(musicResponse.tracks.track[randomNo].url)
-			$(newDiv).append(`<a href=${musicResponse.tracks.track[randomNo].url}> ${musicResponse.tracks.track[randomNo].artist.name +", "+ musicResponse.tracks.track[randomNo].name}</a>`);
-			$('#resultsDiv').prepend(newDiv);		
+			for (var i = 0; i < 9; i++) {
+				songsDiv.append(`<a href=${musicResponse.tracks.track[i].url} target="_blank"> ${musicResponse.tracks.track[i].artist.name +", "+ musicResponse.tracks.track[i].name}</a><br>`);
+				$('#resultsDiv').html(newDiv);
+				$('#displaySongs').html(songsDiv);	
+			}	
 		})
 	}else if(mostFitting[1]==="sadness"||mostFitting[1]==="fear"){
 		var queryURL = "http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=sad&limit=3&api_key=b164494922abda22f8cd2e53cc25ab4e&format=json"
@@ -103,9 +112,12 @@ $('#submitImgUrl').on('click',function(){
 			var musicResponse = response
 			console.log("success got data", musicResponse);
 			var randomNo = Math.floor(Math.random()*10);
-			console.log(musicResponse.tracks.track[randomNo].url)
-			$(newDiv).append(`<a href=${musicResponse.tracks.track[randomNo].url}> ${musicResponse.tracks.track[randomNo].artist.name +", "+ musicResponse.tracks.track[randomNo].name}</a>`);
-			$('#resultsDiv').prepend(newDiv);		
+			for (var i = 0; i < 9; i++) {
+				songsDiv.append(`<a href=${musicResponse.tracks.track[i].url} target="_blank"> ${musicResponse.tracks.track[i].artist.name +", "+ musicResponse.tracks.track[i].name}</a><br>`);
+				$('#resultsDiv').html(newDiv);
+				$('#displaySongs').html(songsDiv);	
+			}
+				
 		})
 	}else{
 		var queryURL = "http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=chill&limit=3&api_key=b164494922abda22f8cd2e53cc25ab4e&format=json"
@@ -117,12 +129,86 @@ $('#submitImgUrl').on('click',function(){
 			var musicResponse = response
 			console.log("success got data", musicResponse);
 			var randomNo = Math.floor(Math.random()*10);
-			console.log(musicResponse.tracks.track[randomNo].url)
-			$(newDiv).append(`<a href=${musicResponse.tracks.track[randomNo].url}> ${musicResponse.tracks.track[randomNo].artist.name +", "+ musicResponse.tracks.track[randomNo].name}</a>`);
-			$('#resultsDiv').prepend(newDiv);		
+			for (var i = 0; i < 9; i++) {
+				songsDiv.append(`<a href=${musicResponse.tracks.track[i].url} target="_blank"> ${musicResponse.tracks.track[i].artist.name +", "+ musicResponse.tracks.track[i].name}</a><br>`);
+				$('#resultsDiv').html(newDiv);
+				$('#displaySongs').html(songsDiv);	
+			}		
 		})
 	};
 	});
 	$('#inputImgUrl').val('');
 
 });
+
+// $("#happy").on("click", function() {
+// 	var newDiv = $('<div>');
+// 	var userUrl = $('#inputImgUrl').val().trim();
+// 	var APIKey='dae956b6026544528d9a145149191299';
+// 	var APISecret = '3d1c30b4f5d746dab0f467e44a1041f4';
+// 	var settings = {
+//   "async": true,
+//   "crossDomain": true,
+//   "url": "http://api.skybiometry.com/fc/faces/detect?api_key="+APIKey+"&api_secret="+APISecret+"&urls="+userUrl+"&attributes=all",
+//   "method": "POST",
+//   "headers": {
+//     "cache-control": "no-cache",
+//     "postman-token": "7cfb77df-6049-346b-87d8-63ba7649762f"
+//   }
+// 	}
+
+// 	$.ajax(settings).done(function (response) {
+// 		var resp = response;
+// 		console.log(resp);
+// 		var respObj = {
+// 			anger:resp.photos[0].tags[0].attributes.anger,
+// 			disgust:resp.photos[0].tags[0].attributes.disgust,
+// 			fear:resp.photos[0].tags[0].attributes.fear,
+// 			happiness:resp.photos[0].tags[0].attributes.happiness,
+// 			neutral_mood:resp.photos[0].tags[0].attributes.neutral_mood,
+// 			sadness:resp.photos[0].tags[0].attributes.sadness,
+// 			surprise:resp.photos[0].tags[0].attributes.surprise
+// 		};
+// 		console.log(respObj);
+
+// 		//create a div with the submitted image and the product of the analysis
+// 		var newDiv = $('<div>');
+// 		var newImg = $('<img>');
+// 		$(newImg).attr('src',userUrl);
+// 		$(newImg).attr('width',320);
+// 		$(picDisplay).html(newImg);
+
+// 		$.ajax({
+// 			url: queryURL,
+// 			method: "GET"
+// 		}).done(function(response){
+// 			var musicResponse = response
+// 			console.log("success got data", musicResponse);
+// 			var randomNo = Math.floor(Math.random()*10);
+// 			console.log(musicResponse.tracks.track[randomNo].url)
+// 			$(newDiv).append(`<a href=${musicResponse.tracks.track[randomNo].url} target="_blank"> ${musicResponse.tracks.track[randomNo].artist.name +", "+ musicResponse.tracks.track[randomNo].name}</a>`);
+// 			$('#resultsDiv').html(newDiv);		
+// 		})
+// 	});
+
+// 		var queryURL = "http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=sad&limit=3&api_key=b164494922abda22f8cd2e53cc25ab4e&format=json"
+// 		var randomNo = Math.floor(Math.random()*10);
+
+// 		$.ajax({
+// 			url: queryURL,
+// 			method: "GET"
+// 		}).done(function(response){
+// 			var musicResponse = response
+// 			console.log("success got data", musicResponse);
+// 			var randomNo = Math.floor(Math.random()*10);
+// 			// console.log(musicResponse.tracks.track[randomNo].queryURL)
+// 			$(newDiv).append(`<a href=${musicResponse.tracks.track[randomNo].url} target="_blank"> ${musicResponse.tracks.track[randomNo].artist.name +", "+ musicResponse.tracks.track[randomNo].name}</a>`);
+// 			$('#resultsDiv').html(newDiv);		
+// 		})
+
+// 	});
+
+
+
+
+
